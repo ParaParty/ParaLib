921404e7a56baacbd7a4b60618e8bc2105eff5b3
hytmiao <hytmiao@outlook.com>
2018-02-17 18:05:14 +0800

    ·不明bug：传一个x认为是table

--- ParaLib.lua
+++ ParaLib.lua
@@ -3,106 +3,46 @@
 Many useful lib:Vector,Blocks,etc.
 by Hyt
 ]]--
-ParaLib = {SBlockList = {},Vector = Vector,SBlock = SBlock,SBlockGroup = SBlockGroup};
-ParaLib.__index = ParaLib;
+ParaLib = {};
 function main(entity)
-    script = blocks.getscript(19200,1,19200);
+    local script = blocks.getscript(19200,1,19200);
     script.ParaLib = ParaLib;
-    cmd("/tip -color #0000ff 【ParaLib】 已载入");
+    ParaLib.SetBlocks = SetBlocks;
+    ParaLib.Translate = Translate;
+    cmd("/tip -notice -color #0000cc 【ParaLib】已载入");
 end
 
---VectorLib
-Vector = {x = 0, y = 0, z = 0,
-    __add = function(v1,v2)
-        local result = NewVector(0,0,0);
-        result.x = v1.x + v2.x;
-        result.y = v1.y + v2.y;
-        result.z = v1.z + v2.z;
-        return result;
-    end,
-    __sub = function(v1,v2)
-        local result = NewVector(0,0,0);
-        result.x = v1.x - v2.x;
-        result.y = v1.y - v2.y;
-        result.z = v1.z - v2.z;
-        return result;
-    end
-}
-Vector.__index = Vector;
-function Vector:Multiply(num)
-    self.x  = self.x * num;
-    self.y  = self.y * num;
-    self.z  = self.z * num;
-end
-
---turn the vector into string
-function Vector:ToString()
-    return self.x.." "..self.y.." "..self.z;
-end
---turn the vectot into parm
-function Vector:ToParm()
-    return self.x, self.y, self.z;
-end
---Newvector
-function ParaLib:NewVector(x,y,z)
-    local result = {__index = Vector};
-    --assign the value
-    result.x = x;
-    result.y = y;
-    result.z = z;
-    return result;
-end
-
---set a group of block
-function ParaLib:SetBlocks(pos,range,id,data,flag)
-    for i = pos.x, pos.x + range.rx do
-        for j = pos.y, pos.y + range.ry do
-            for k = pos.z, pos.z + range.rz do
-                BlockEngine.SetBlock(i,j,k,id,data,flag);
+--Set a box of blocks
+function SetBlocks(x, y, z, rx, ry, rz, id, data, flag)
+    id = id or 0;
+    data = data or 0;
+    flag = flag or 0;
+    for i = x, x + rx do
+        for j = y, y + ry do
+            for k = z, z + rz do 
+                BlockEngine:SetBlock(i, j, k, id, data, flag);
             end
         end
     end
 end
 
-SBlock = {Position = {0, 0, 0}, Id = 0, Data = 0};
-SBlock.__index = SBlock;
---create a smart block
-function ParaLib:CreateSBlock(x,y,z)
-    local newBlock = {__index = SBlock};
-    --初始化一堆信息
-    newBlock.Position = ParaLib:NewVector(x,y,z);
-    --注册到表里
-    table.insert(ParaLib.SBlockList,newBlock);
-    return newBlock;
+--Translate (a box of) block(s)
+function Translate(x, y, z, rx, ry, rz, tx, ty, tz)
+    DoBlocks(x, y, z, rx, ry, rz,
+        function(x,y,z,tx,ty,tz)
+            cmd("/translate "..x.." "..y.." "..z.." to "..tx.." "..ty.." "..tz);
+        end,
+        tx,ty,tz);
 end
 
---获取Id
-function SBlock:GetId()
-    return BlockEngine:GetBlockId(self.Position:ToParm());
-end
-
---获取Data
-function SBlock:GetData()
-    return BlockEngine:GetBlockData(self.Position:ToParm());
-end
-
---Move a Block relatively
-function SBlock:SimpleMoveRelative(x,y,z,newData)
-    cmd("/translate ~ ~ ~ to "..ParaLib:NewVector(x,y,z):ToString());
-    self.Position = self.Position + NewVector(x,y,z);
-    if not newData then
-        BlockEngine:SetBlockData(self.Position:ToParm(),newData,0);
+--Do (a box of) block(s)
+--parm1~3: some extra parameter.
+function DoBlocks(x, y, z, rx, ry, rz, command, parm1, parm2, parm3)
+    for i = x, x + rx do
+        for j = y, y + ry do
+            for k = z, z + rz do 
+                command(i, j, k, parm1, parm2, parm3);
+            end
+        end
     end
-end
-
---Move a Block absolately
-function SBlock:SimpleMoveAbsolate(x,y,z,newData)
-    self:SimpleMoveRelative(ParaLib:NewVector( x,y,z) - self.Position,newData);
-end
-
-SBlockGroup = {};
-SBlockGroup.__index = SBlockGroup;
---Move all SBlock in the group
-function SBlockGroup:SimpleMoveGroupRelative(to)
-
 end
